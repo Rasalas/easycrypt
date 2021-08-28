adcf6c470ba8c8f104113523d8269c5e06ac67d8
rasalas <Rasalas@users.noreply.github.com>
2021-08-28 21:43:02 +0200

    💄 show it working

--- index.php
+++ index.php
@@ -7,13 +7,29 @@ $ez = new EasyCrypt('supersecurepassphrase', 'saltandpepper');
 $data = ['user_id'=>'12','firstname'=>'John', 'lastname'=>'Doe'];
 
 $crypt = $ez->encrypt($data);
+$crypt_urlsafe = $ez->encrypt($data,URL_SAFE);
+
+echo '<h3>Original data:</h3>';
+json_view($data);
+echo '<br>';
+echo '<h3>ENCRYPTED:</h3>';
+echo '<textarea rows=4 style="width:100%">'.$crypt.'</textarea>';
+
+echo '<h3>DECRYPTED:</h3>';
+json_view($ez->decrypt($crypt),JSON_PRETTY_PRINT);
 
 
-var_dump($data) ;
 echo '<br>';
-echo $crypt;
+echo '<h3>ENCRYPTED (url safe):</h3>';
+echo '<textarea rows=4 style="width:100%">'.$crypt_urlsafe.'</textarea>';
 echo '<br>';
-echo strlen($crypt);
+echo '<h3>DECRYPTED (url safe):</h3>';
+json_view($ez->decrypt($crypt_urlsafe,URL_SAFE));
+
 echo '<br>';
-var_dump($ez->decrypt($crypt));
+
+function json_view($array){
+    $rows = count($array) + 2;
+    echo '<textarea rows='.$rows.' style="width:100%">'.json_encode($array,JSON_PRETTY_PRINT).'</textarea>';
+}
 
