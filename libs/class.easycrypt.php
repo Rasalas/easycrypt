f7c7e0c646c953155763bc6e39fe9b6c9366111a
rasalas <Rasalas@users.noreply.github.com>
2021-08-28 23:10:35 +0200

    📝 added docs

--- libs/class.easycrypt.php
+++ libs/class.easycrypt.php
@@ -1,7 +1,31 @@
 <?php
+
 namespace Rasalas\Tools;
+
 define('URL_SAFE', true);
 
+/**
+ * EasyCrypt_Class is a class to easily encrypt data with a simple passphrase
+ * 
+ * EasyCrypt_Class is a class to encrypt and decrypt array data, which 
+ * you can append to your URL. If someone tampers with it, you'll notice.
+ * DO NOT use this to store user data or other sensitive data.
+ * Mainly use it for short term use - e.g. to encrypt a user_id in a GET request
+ * 
+ * Example usage:
+ * $data = ['user_id'=>'12','firstname'=>'John', 'lastname'=>'Doe'];
+ * 
+ * $ez = new Rasalas\Tools\EasyCrypt('supersecurepassphrase', 'saltandpepper');
+ * $crypt = $ez->encrypt($data);
+ * 
+ * echo $ez->decrypt($crypt);
+ * 
+ * @package EasyCrypt
+ * @author  Torben Buck <mail@tbuck.de>
+ * @version 1.0.0
+ * @access  public
+ * @see     https://www.tbuck.de/example/easycrypt
+ */
 class EasyCrypt
 {
     private const ENCRYPTION_ALGORITHM = 'AES-128-ECB';
@@ -9,12 +33,26 @@ class EasyCrypt
     private $key = '';
     private $salt = '';
 
+    /**
+     * Create a new instance of EasyCrypt
+     *
+     * @param  String $key to encrypt/decrypt the data
+     * @param  String $salt to sign the data
+     * @return void
+     */
     public function __construct($key, $salt)
     {
         $this->$key = $key;
         $this->$salt = $salt;
     }
 
+    /**
+     * encrypt data
+     *
+     * @param  array|String $data
+     * @param  boolean $url_safe use URL_SAFE = true for a String without special chars (longer)
+     * @return String encrypted string 
+     */
     public function encrypt($data, $url_safe = false)
     {
         $data_arr = [];
@@ -24,7 +62,7 @@ class EasyCrypt
         } else {
             $data_arr = $data;
         }
-        
+
         $data_arr = serialize($this->sign_data($data_arr));
 
         $encrypted = openssl_encrypt($data_arr, self::ENCRYPTION_ALGORITHM, $this->key);
@@ -34,20 +72,27 @@ class EasyCrypt
         return $encrypted;
     }
 
+    /**
+     * decrypt encrypted string
+     *
+     * @param  String $string
+     * @param  boolean $url_safe use URL_SAFE = true if it was encrypted using URL_SAFE (this is faster but works without it)
+     * @return array|String depending on what type of data it got
+     */
     public function decrypt($string, $url_safe = false)
     {
-                
+
         $data = unserialize(openssl_decrypt($string, self::ENCRYPTION_ALGORITHM, $this->key));
-        
-        if(!is_array($data) || $url_safe){
+
+        if (!is_array($data) || $url_safe) {
             $data = base64_decode(rawurldecode($string));
             $data = unserialize(openssl_decrypt($data, self::ENCRYPTION_ALGORITHM, $this->key));
         }
 
-        if(is_array($data) && $this->check_signature($data)){
+        if (is_array($data) && $this->check_signature($data)) {
             $data = $this->strip_signature($data);
             $keys = array_keys($data);
-            if(count($data) == 1 && $this->surroundedBy($keys[0],self::DELIMITER)){
+            if (count($data) == 1 && $this->surroundedBy($keys[0], self::DELIMITER)) {
                 return $data[$keys[0]];
             }
             return $data;
@@ -55,6 +100,12 @@ class EasyCrypt
         return false;
     }
 
+    /**
+     * sign_data hashes the contents of the array and adds a signature to the array
+     *
+     * @param  array $data
+     * @return array with signature
+     */
     private function sign_data(array $data)
     {
         $salted_array = array_merge($data, ['salt' => $this->salt]);
@@ -64,9 +115,15 @@ class EasyCrypt
         return $signed_data;
     }
 
-    private function check_signature(array $data)
+    /**
+     * check_signature checks if the signature is equal to the hash of the array
+     *
+     * @param  array $data
+     * @return boolean
+     */
+    private function check_signature(array $data): bool
     {
-        if(!isset($data['signature'])) return false;
+        if (!isset($data['signature'])) return false;
 
         $data_signature = $data['signature'];
         unset($data['signature']);
@@ -79,27 +136,55 @@ class EasyCrypt
         return false;
     }
 
+    /**
+     * strip_signature strips the array of the appended signature
+     *
+     * @param  array $data
+     * @return array
+     */
     private function strip_signature(array $data): array
     {
         unset($data['signature']);
         return $data;
     }
 
-    // UTIL
-    private function surroundedBy($string, $delimiter){
-        return $this->startsWith($string,$delimiter) && $this->endsWith($string,$delimiter);
+    // UTIL    
+    /**
+     * surroundedBy checks if a string starts and ends with a specific delimiter
+     *
+     * @param  String $string
+     * @param  String $delimiter
+     * @return void
+     */
+    private function surroundedBy($string, $delimiter)
+    {
+        return $this->startsWith($string, $delimiter) && $this->endsWith($string, $delimiter);
     }
 
+    /**
+     * startsWith tells if a string starts with a string
+     *
+     * @param  String $haystack
+     * @param  String $needle
+     * @return boolean
+     */
     private function startsWith($haystack, $needle)
     {
         $length = strlen($needle);
         return substr($haystack, 0, $length) === $needle;
     }
 
-    private function endsWith( $haystack, $needle ) 
+    /**
+     * endsWith tells if a string ends with a string
+     *
+     * @param  String $haystack
+     * @param  String $needle
+     * @return boolean
+     */
+    private function endsWith($haystack, $needle)
     {
-        $length = strlen( $needle );
-        if( !$length ) return true;
-        return substr( $haystack, -$length ) === $needle;
+        $length = strlen($needle);
+        if (!$length) return true;
+        return substr($haystack, -$length) === $needle;
     }
 }
