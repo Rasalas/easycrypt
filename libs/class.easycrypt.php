4c317ae1827bf30c148ffdaab4db0919698bea58
rasalas <Rasalas@users.noreply.github.com>
2021-08-29 15:12:17 +0200

    🐛 key collisions now impossible

--- libs/class.easycrypt.php
+++ libs/class.easycrypt.php
@@ -2,7 +2,9 @@
 
 namespace Rasalas\Tools;
 
-define('URL_SAFE', true);
+define('URL_UNSAFE', 0);
+define('URL_SAFE', 1);
+
 
 /**
  * EasyCrypt_Class is a class to easily encrypt data with a simple passphrase
@@ -34,6 +36,9 @@ class EasyCrypt
 {
     private const ENCRYPTION_ALGORITHM = 'AES-128-ECB';
     private const DELIMITER = '----';
+    private const KEY_DATA = 'data';
+    private const KEY_SIGNATURE = 'signature';
+    private const KEY_SALT = 'salt';
     private $key = '';
     private $salt = '';
 
@@ -57,15 +62,10 @@ class EasyCrypt
      * @param  bool $url_safe use URL_SAFE = true for a String without special chars (longer)
      * @return String encrypted string 
      */
-    public function encrypt($data, bool $url_safe = false)
+    public function encrypt($data, bool $url_safe = URL_UNSAFE)
     {
         $data_arr = [];
-
-        if (!is_array($data)) {
-            $data_arr[$this->wrap(uniqid())] = $data;
-        } else {
-            $data_arr = $data;
-        }
+        $data_arr[self::KEY_DATA] = $data;
 
         $data_arr = serialize($this->sign_data($data_arr));
 
@@ -83,9 +83,8 @@ class EasyCrypt
      * @param  bool $url_safe use URL_SAFE = true if it was encrypted using URL_SAFE (this is faster but works without it)
      * @return array|String depending on what type of data it got
      */
-    public function decrypt(String $string, bool $url_safe = false)
+    public function decrypt(String $string, bool $url_safe = URL_UNSAFE)
     {
-
         $data = unserialize(openssl_decrypt($string, self::ENCRYPTION_ALGORITHM, $this->key));
 
         if (!is_array($data) || $url_safe) {
@@ -112,10 +111,10 @@ class EasyCrypt
      */
     private function sign_data(array $data): array
     {
-        $salted_array = array_merge($data, ['salt' => $this->salt]);
+        $salted_array = array_merge($data, [self::KEY_SALT => $this->salt]);
         $signature = md5(serialize($salted_array));
 
-        $signed_data = array_merge($data, [$this->wrap('signature') => $signature]);
+        $signed_data = array_merge($data, [self::KEY_SIGNATURE => $signature]);
         return $signed_data;
     }
 
@@ -127,13 +126,12 @@ class EasyCrypt
      */
     private function check_signature(array $data): bool
     {
-        $signature = $this->wrap('signature');
-        if (!isset($data[$signature])) return false;
+        if (!isset($data[self::KEY_SIGNATURE])) return false;
 
-        $data_signature = $data[$signature];
-        unset($data[$signature]);
+        $data_signature = $data[self::KEY_SIGNATURE];
+        unset($data[self::KEY_SIGNATURE]);
 
-        $salted_array = array_merge($data, ['salt' => $this->salt]);
+        $salted_array = array_merge($data, [self::KEY_SALT => $this->salt]);
         $temp_signature = md5(serialize($salted_array));
 
         if ($data_signature === $temp_signature) return true;
@@ -149,8 +147,7 @@ class EasyCrypt
      */
     private function strip_signature(array $data): array
     {
-        unset($data[$this->wrap('signature')]);
-        return $data;
+        return $data[self::KEY_DATA];
     }
 
     // UTIL    
@@ -171,7 +168,7 @@ class EasyCrypt
      * @param  String $delimiter
      * @return bool
      */
-    private function surroundedBy(String $string,String $delimiter): bool
+    private function surroundedBy(String $string, String $delimiter): bool
     {
         return $this->startsWith($string, $delimiter) && $this->endsWith($string, $delimiter);
     }
